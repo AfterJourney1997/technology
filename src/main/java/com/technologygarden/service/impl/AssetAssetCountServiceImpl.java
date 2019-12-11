bd848c7a8a863dd2ce020e0cb500132de3feab01
孔嘉培 <1321516496@qq.com>
2019-12-12 00:22:50 +0800

    修改房产无法获取list的bug。

---
+++ src/main/java/com/technologygarden/service/impl/AssetAssetCountServiceImpl.java
@@ -0,0 +1,8 @@
+package com.technologygarden.service.impl;
+
+import com.technologygarden.service.AssetAssetCountService;
+import org.springframework.stereotype.Service;
+
+@Service("assetAssetCountService")
+public class AssetAssetCountServiceImpl implements AssetAssetCountService {
+}
